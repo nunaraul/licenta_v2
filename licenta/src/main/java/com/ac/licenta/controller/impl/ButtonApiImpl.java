cb7fdcb06f6c950f50cbd41a1dca0786bbbc882a
nunar <nunaraulltc@gmail.com>
2020-01-16 10:31:46 +0200

    Button corrected added

---
+++ licenta/src/main/java/com/ac/licenta/controller/impl/ButtonApiImpl.java
@@ -0,0 +1,37 @@
+package com.ac.licenta.controller.impl;
+
+import com.ac.licenta.controller.ButtonApi;
+import com.ac.licenta.dto.ButtonDTO;
+import com.ac.licenta.service.ButtonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ButtonApiImpl implements ButtonApi {
+
+    private final ButtonService buttonService;
+
+    @Autowired
+    public ButtonApiImpl(ButtonService buttonService){
+        this.buttonService = buttonService;
+    }
+
+    @Override
+    public List<ButtonDTO> list(){
+        return buttonService.list();
+    }
+
+    @Override
+    public ButtonDTO findById(@PathVariable("id") Long id){
+        return buttonService.findById(id);
+    }
+
+    @Override
+    public ButtonDTO click(@PathVariable("id") Long id){
+        return buttonService.click(id);
+    }
+
+}
